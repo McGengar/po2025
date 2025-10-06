1cd569da58065ef9cb150931c9dc77fe6e03646b
McGengar <macwegrzyn@gmail.com>
2025-10-06 15:55:03 +0200

    Zajęcia 1:Lotto

---
+++ Lab1/Lab1/src/Lotto.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Lotto {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Please enter six numbers between 1 and 49");
+        int[] inputs = new int[6];
+        int[] numbers = new int[6];
+        Random rand = new Random();
+        for (int i = 0; i < 6; i++) {
+            inputs[i] = input.nextInt();
+            numbers[i] = rand.nextInt(49)+1;
+        }
+        System.out.print("Wygrywające liczby w Lotto to: ");
+        for (int i = 0; i < 6; i++) {
+            System.out.print(numbers[i]);
+            System.out.print(" ");
+        }
+
+    }
+}
