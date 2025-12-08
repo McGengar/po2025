07812045f8e10a205a77c873f8fb36a619aa1147
McGengar <macwegrzyn@gmail.com>
2025-12-08 19:05:21 +0100

    Dodano dzialajacy formularz

--- SymulatorGui/src/main/java/org/example/symulatorgui/DodajSamochodController.java
+++ SymulatorGui/src/main/java/org/example/symulatorgui/DodajSamochodController.java
@@ -1,4 +1,70 @@
 package org.example.symulatorgui;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DodajSamochodController {
+    @FXML
+    public TextField modelField;
+    @FXML
+    public TextField regNumberField;
+    @FXML
+    public TextField weightField;
+    @FXML
+    public TextField MaxSpeedField;
+    public ComboBox engineComboBox;
+    public ComboBox gearboxComboBox;
+    public Button confirmButton;
+    public Button cancelButton;
+
+    @FXML
+    private void onConfirmButton() throws IOException {
+        String model = modelField.getText();
+        String registration = regNumberField.getText();
+        double weight;
+        int speed;
+        try {
+            weight = Double.parseDouble(weightField.getText());
+            speed = Integer.parseInt(MaxSpeedField.getText());
+        } catch (NumberFormatException e) {
+            System.out.println("Niepoprawne dane. Spróbuj ponownie.");
+            return;
+        }
+        HelloController.addCarToList(model, registration, weight, speed);
+        Stage stage = (Stage) confirmButton.getScene().getWindow();
+        stage.close();
+
+    }
+
+    @FXML
+    private void onCancelButton() throws IOException {
+        Stage stage = (Stage) cancelButton.getScene().getWindow();
+        stage.close();
+    }
+    @FXML
+    public void initialize(){
+        cancelButton.setOnAction(event -> {
+            //current_samochod = AddNewCar();
+            try {
+                onCancelButton();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+        confirmButton.setOnAction(event -> {
+            //current_samochod = AddNewCar();
+            try {
+                onConfirmButton();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+
+    }
 }
--- SymulatorGui/src/main/java/org/example/symulatorgui/HelloController.java
+++ SymulatorGui/src/main/java/org/example/symulatorgui/HelloController.java
@@ -2,17 +2,23 @@ package org.example.symulatorgui;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 
-import javafx.scene.control.MenuItem;
-import javafx.scene.control.TextField;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
+import javafx.stage.Stage;
 import symulator.*;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloController {
     public Samochod current_samochod;
+
 
     public MenuItem ContexMenuOpen;
     public MenuItem ContextMenuSave;
@@ -57,17 +63,20 @@ public class HelloController {
     public Button ClutchEngageButton;
     public Button ClutchDisengageButton;
     public ImageView carImageView;
+    public ComboBox CarSelectionComboBox;
+
+    public static ArrayList<Samochod> samochody = new  ArrayList<>();
 
     @FXML
     public void initialize(){
 
-        Silnik silnik = new Silnik(6700,0,6700,670,"ssb silnik","SSB","SuperSzybki67");
-        Sprzeglo sprzeglo = new Sprzeglo(5000,300,"Uuusprzeglo","UuuEntprice","Sigma");
-        SkrzyniaBiegow skrzyniaBiegow = new SkrzyniaBiegow(sprzeglo,0,6,2500,40,"SkrzyniaAlfa","Wilki","Auu");
-        Pozycja pozycja= new Pozycja(21,37);
-
-        Samochod honda_szybsza_niz_wygloada = new Samochod(pozycja,silnik,skrzyniaBiegow,true,213767,"Civic",167);
-        current_samochod = honda_szybsza_niz_wygloada;
+//        Silnik silnik = new Silnik(100,0,100,10,"ssb silnik","SSB","SuperSzybki1");
+//        Sprzeglo sprzeglo = new Sprzeglo(5000,300,"Uuusprzeglo","UuuEntprice","Sigma");
+//        SkrzyniaBiegow skrzyniaBiegow = new SkrzyniaBiegow(sprzeglo,0,6,2500,40,"SkrzyniaAlfa","Wilki","Auu");
+//        Pozycja pozycja= new Pozycja(21,37);
+//
+//        Samochod honda_szybsza_niz_wygloada = new Samochod(pozycja,silnik,skrzyniaBiegow,true,21371,"Civic",11);
+        current_samochod = new Samochod();
         refresh();
 
         Image carImage = new Image(getClass().getResource("/honda.png").toExternalForm());
@@ -80,20 +89,63 @@ public class HelloController {
 
 
         AddNewCarButton.setOnAction(event -> {
-            current_samochod = AddNewCar();
+            //current_samochod = AddNewCar();
+            try {
+                openAddCarWindow();
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
             refresh();
 
         });
+
+        CarSelectionComboBox.valueProperty().addListener((_, _, _) -> {
+            current_samochod = samochody.get(CarSelectionComboBox.getSelectionModel().getSelectedIndex());
+            refresh();
+        });
+
+    }
+    @FXML
+    public static void addCarToList(String model, String registration, double weight, int speed) {
+        Silnik silnik = new Silnik(100,0,100,10,"ssb silnik","SSB","SuperSzybki1");
+        Sprzeglo sprzeglo = new Sprzeglo(5000,300,"Uuusprzeglo","UuuEntprice","Sigma");
+        SkrzyniaBiegow skrzyniaBiegow = new SkrzyniaBiegow(sprzeglo,0,6,2500,40,"SkrzyniaAlfa","Wilki","Auu");
+        Pozycja pozycja= new Pozycja(0,0);
+        Samochod new_samochod = new Samochod(pozycja,silnik,skrzyniaBiegow,false,Integer.valueOf(registration),model,speed);
+        samochody.add(new_samochod);
     }
 
+    @FXML
     public Samochod AddNewCar(){
         System.out.println("AddNewCar");
         Samochod current_samochod = new Samochod();
         return new Samochod();
 
     }
+    @FXML
+    public void openAddCarWindow() throws IOException {
+        FXMLLoader loader = new
+                FXMLLoader(getClass().getResource("DodajSamochod.fxml"));
+        Stage stage = new Stage();
+        try {
+            stage.setScene(new Scene(loader.load()));
+        }
+        catch (IOException e) {
+            System.out.println("Nie powiodło się. Spróbuj ponownie.");
+            return;
+        }
+        stage.setTitle("Dodaj nowy samochód");
+        stage.show();
 
-    void refresh(){
+    }
+    @FXML
+    public void refresh(){
+        CarSelectionComboBox.getItems().clear();
+        for(Samochod samochod : samochody){
+            CarSelectionComboBox.getItems().add(String.valueOf(samochod.getModel()));
+        }
+
+
         CarModelTextField.setText(String.valueOf(current_samochod.getModel()));
         CarRegNrTextField.setText(String.valueOf(current_samochod.getNrRejest()));
         CarWeightTextField.setText(String.valueOf(current_samochod.getWaga()));
@@ -114,7 +166,7 @@ public class HelloController {
         ClutchPriceTextField.setText(String.valueOf(current_samochod.getSkrzynia().getSprzeglo().getCena()));
         ClutchWeightTextField.setText(String.valueOf(current_samochod.getSkrzynia().getSprzeglo().getWaga()));
         ClutchStatusTextField.setText(String.valueOf(current_samochod.getSkrzynia().getSprzeglo().getStanSprzegla()));
-
+        current_samochod.getPozycja().setX(current_samochod.getPozycja().getX()+20);
         carImageView.setTranslateX(current_samochod.getPozycja().getX());
         carImageView.setTranslateY(current_samochod.getPozycja().getY());
 
